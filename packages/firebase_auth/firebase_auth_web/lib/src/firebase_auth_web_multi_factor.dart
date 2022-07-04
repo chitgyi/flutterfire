8acc727deb1157bd4fdfc5ea96354b1b6932f5e5
Guillaume Bernos <guillaume@bernos.dev>
2022-07-04 14:17:09 +0200

    feat(auth, web): add phone mfa

--- packages/firebase_auth/firebase_auth_web/lib/src/firebase_auth_web_multi_factor.dart
+++ packages/firebase_auth/firebase_auth_web/lib/src/firebase_auth_web_multi_factor.dart
@@ -4,8 +4,20 @@
 // found in the LICENSE file.
 
 import 'package:firebase_auth_platform_interface/firebase_auth_platform_interface.dart';
+import 'package:firebase_core_web/firebase_core_web_interop.dart'
+    as core_interop;
+
+import 'interop/auth.dart' as auth_interop;
 
 /// Web delegate implementation of [UserPlatform].
 class MultiFactorWeb extends MultiFactorPlatform {
   MultiFactorWeb(FirebaseAuthPlatform auth) : super(auth);
+
+  /// instance of Auth from the web plugin
+  auth_interop.Auth? _webAuth;
+
+  auth_interop.Auth get _delegate {
+    return _webAuth ??=
+        auth_interop.getAuthInstance(core_interop.app(auth.app.name));
+  }
 }
